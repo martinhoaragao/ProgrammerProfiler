1c334f14a50135140640d3106ac6042a41a7ac13
Martinho Aragão <martinhoaragao@gmail.com>
2019-03-10 12:48:45 +0000

    Add auxiliar files

--- ExerciseExamples/Ex2_P2_Idades/P/ExIdades_P.java
+++ ExerciseExamples/Ex2_P2_Idades/P/ExIdades_P.java
@@ -7,25 +7,30 @@ public class ExIdades_Teste {
         int n, m, idade;
         int total=0;
         float media;
+
         do {
             System.out.println("Introduza um numero para comparacao");
             m = ler.nextInt();
         } while (m <=0);
+
         do {
             System.out.println("Introduza o numero de idades");
             n = ler.nextInt();
         } while (n <= 0);
+
+        // Read the age values
         for (int i = 0; i < n; i++) {
-            
             idade = ler.nextInt();
+
             if (idade > m) {
                 System.out.println(idade);
             }
+
             total=total+idade;
         }
+
+        //The division result must be casted to float!
         media=(float)total/n;
         System.out.println(media);
-
-}
-
+    }
 }
